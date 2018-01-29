4fe5e93a873c22a3739822180320350195bcb16a
Keisuke Kuribayashi <montblanc.notdeliciosu@gmail.com>
2018-01-29 16:50:47 +0900

    検索結果画面にListViewを追加、1〜10の番号を表示する処理を追加。 また各行をクリックした時のイベント処理を実装

--- app/src/main/java/kuririnz/xyz/bookdiscovery/ResultListActivity.java
+++ app/src/main/java/kuririnz/xyz/bookdiscovery/ResultListActivity.java
@@ -1,13 +1,50 @@
 package kuririnz.xyz.bookdiscovery;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ResultListActivity extends AppCompatActivity {
+
+    // xmlファイルのコンポーネントと関連付ける要素
+    ListView resultListView;
+    // 検証用コレクションデータ
+    List<String> listData = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_list);
+
+        // xmlファイルのコンポーネントと関連付け
+        resultListView = findViewById(R.id.ResultListView);
+        // ListViewに表示する情報をまとめるAdapterをインスタンス化
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(ResultListActivity.this
+                , android.R.layout.simple_list_item_1
+                , listData);
+        // ListViewに表示情報をまとめたAdapterをセット
+        resultListView.setAdapter(adapter);
+
+        // ListViewの各行をクリックした時の命令を実装
+        AdapterView.OnItemClickListener listItemClickEvent = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // クリックした行番号をToastで表示する
+                Toast.makeText(ResultListActivity.this
+                        , i + "行目をクリックしました"
+                        , Toast.LENGTH_SHORT).show();
+            }
+        };
+        // ListViewに行をクリックした時のイベントを登録
+        resultListView.setOnItemClickListener(listItemClickEvent);
     }
 }
