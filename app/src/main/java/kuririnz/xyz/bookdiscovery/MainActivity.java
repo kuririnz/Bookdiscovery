f3728704ce77c9879dfa34ddbe91b612da5b93da
Keisuke Kuribayashi <montblanc.notdeliciosu@gmail.com>
2018-02-02 10:40:53 +0900

    タイマースレッドの実装を追加、またタイマースレッドが蔵書検索ボタンを押下時に停止する様処理を修正。

--- app/src/main/java/kuririnz/xyz/bookdiscovery/MainActivity.java
+++ app/src/main/java/kuririnz/xyz/bookdiscovery/MainActivity.java
@@ -2,6 +2,7 @@ package kuririnz.xyz.bookdiscovery;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -9,17 +10,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     // レイアウトxmlと関連付けるWidget
     Button bookSearchBtn;
     EditText bookSearchEditor;
+    // Timerクラス
+    Timer timer;
+    // メインスレッドに帰って来るためのハンドラー
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ハンドラーオブジェクトをMainThreadでインスタンス化
+        handler = new Handler();
         // 蔵書検索ボタンをjavaプログラムで操作できるように名前をつける
         bookSearchBtn = findViewById(R.id.BookSearchBtn);
         // 蔵書検索する文字を入力するEditTextをjavaプログラムで操作できるように名前をつける
@@ -34,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext()
                         , "入力された文字は [" + bookSearchEditor.getText().toString() + "]です。"
                         , Toast.LENGTH_LONG).show();
+                // Timerスレッドを止める
+                timer.cancel();
                 // 画面遷移するためのIntentをインスタンス化
                 Intent intent = new Intent(MainActivity.this, ResultListActivity.class);
                 // 画面遷移アクションを実行
@@ -42,5 +54,30 @@ public class MainActivity extends AppCompatActivity {
         };
         // 蔵書検索ボタンが押された時に実行するプログラムをボタンに登録
         bookSearchBtn.setOnClickListener(bookSearchEvent);
+
+        // 準備されているTimerスレッドをインスタンス化
+        timer = new Timer();
+        // ３秒ごとに実行するタスク(TimerTask)をインスタンス化
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                // コンソールログに更新された内容を出力
+                Log.d("SubThread Process"
+                        , "「" +  bookSearchEditor.getText().toString() + "」に更新されました。");
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 更新された内容をトーストに表示
+                        Toast.makeText(getBaseContext()
+                                , "「" +  bookSearchEditor.getText().toString() + "」に更新されました。"
+                                , Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        };
+
+        // Timerスレッドの実行スケジュールを設定
+        // 3秒毎にtimerTaskのプログラムを実行
+        timer.schedule(timerTask, 3000, 3000);
     }
 }
